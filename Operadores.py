6ee182d623358d887eac6614bf2133ee899e33cd
Renato Rosa dos Santos Filho <renatto.rfilho@yahoo.com>
2025-07-14 19:31:36 -0300

    Add files via upload

---
+++ Operadores.py
@@ -0,0 +1,59 @@
+# adição
+adicao = 2 + 2
+print('adição => ', adicao)
+
+
+
+# subtração
+subtracao = 2 - 2
+print('subtração => ', subtracao)
+
+
+
+# divisão
+divisao = 2 / 2
+print('divisão => ', divisao)
+
+
+
+divisao_inteira = 10//3
+print('Divisão => ', divisao_inteira)
+
+
+
+modulo = 55 % 2    # A variável módulo verifica o resto da divisão
+print('Módulo => ', modulo)
+
+
+
+concatenacao = 'Senac' + ' - ' + 'Curso Python'
+print(concatenacao)
+
+
+exponenciacao = 2**10
+print("Exponenciação=> ", exponenciacao)
+
+
+
+# multiplicação
+mutiplicacao = 2 * 2
+print('mutiplicação => ', mutiplicacao)
+
+
+cinco_vezes_a = 5 * 'a'
+print(cinco_vezes_a)
+
+
+
+#Precedencia
+#180 + 20 / 2 => 100
+# ( 180 + 20 ) / 2 => 100
+"""
+1. (x + x)
+2. **
+3. / ou // caso haja as duas. Depos faz se a * e por ultimo a % ( ou a que vier primeiro da esuqerda pra direita / e ou %)
+4. + 
+"""
+
+
+
---
+++ formatacao.py
@@ -0,0 +1,11 @@
+# imc = indice de massa corporal
+# imc = peso / altura ** 2
+# imc = peso / (altura * altura)
+
+nome = 'lucas'
+altura = 1.71
+peso = 80
+imc = peso / altura ** 2 
+print(nome, 'tem' ,altura, 'de altura' ,peso, 'kg' , 'e' ,imc, 'de massa corporal')
+# f - strigs . O que esta dentro das chaves é uma variável e do lado de fora é o rótulo.
+print(f'{nome} tem {altura} de altura e, pesa {peso} e seu IMC é = {imc : .2f}')
