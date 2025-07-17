0c6511f28adb90b71e0a8ce45ba946b8c66cba3c
Renato Rosa dos Santos Filho <renatto.rfilho@yahoo.com>
2025-07-16 21:39:59 -0300

    Add files via upload

--- exec28.py
+++ exec28.py
@@ -1,5 +1,26 @@
-# 16.	Faça um programa para uma loja de tintas. O programa deverá pedir o 
-# tamanho em metros quadrados da área a ser pintada. Considere que a cobertura 
-# da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em 
-# latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de 
-# tinta a serem compradas e o preço total.
+""" 16.	Faça um programa para uma loja de tintas. O programa deverá pedir o 
+ tamanho em metros quadrados da área a ser pintada. Considere que a cobertura 
+ da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em 
+ latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de 
+ tinta a serem compradas e o preço total.
+
+ """
+
+# o tamanho em metros quadrados da área a ser pintada
+ap = float(input(" Informe o valor da area em metros quadrados a ser pintada: "))
+
+# a cobertura da tinta é de 1 litro para cada 3 metros quadrados
+tl = 3
+
+# a tinta é vendida em latas de 18 litros, que custam R$ 80,00
+
+lt = 18
+ltr = 80.00
+
+# Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total
+
+if ap > lt:
+    exc = ap - lt
+    
+
+
